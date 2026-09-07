b26f88a148d65b4e5ba1d386bd1b5778edd668f7
JulieHoyen <juho0001@stud.ek.dk>
2026-09-07 12:30:20 +0200

    Add new features and styles for task management application
    - Introduced a new PNG asset for snow showers and thunder in polar twilight.
    - Implemented utility functions in utils_lib.js for greeting users, generating random numbers, selecting DOM elements, and loading JSON data.
    - Created a new CSS file (to_do.css) with styles for the task management application, including root variables, layout, buttons, and responsive design.
    - Developed the initial structure of the task management functionality in to_do_index.js, including event listeners and task rendering logic.

---
+++ to_do_index.js
@@ -0,0 +1,30 @@
+"use strict"
+
+
+
+
+const taskt_indput = document.querySelector(".task_text");
+const createTask_btn = document.querySelector(".createTask");
+const ul_elm = document
+createTask_btn.addEventListener("click", createTask);
+const task_arr = [];
+const ul_elm =[];
+function createTask() {
+createTask_btn.addEventListener("clik")
+
+    console.log ("hello")
+
+    const task_obj = (taskTxt: taskt_indput.value, )
+}
+
+
+
+
+
+function renderList() {
+task_arr.forEach((task) => {
+
+ul_elm.innerHTML += `<li><p>${task.taskTxt}</p></li>``
+});
+
+}
